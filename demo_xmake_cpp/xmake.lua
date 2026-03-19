856cc8bf4612c8166cdce2643479267421e460d5
sunrisepeak <speakshen@163.com>
2026-03-19 10:27:15 +0800

    refactor: drop manual rule import boilerplate from xmake example

--- demo_xmake_cpp/xmake.lua
+++ demo_xmake_cpp/xmake.lua
@@ -2,28 +2,6 @@ set_project("demo_xmake_cpp")
 set_version("0.1.0")
 set_languages("cxx17")
 
-local function import_ament_xmake_rule()
-    local injected = os.getenv("AMENT_XMAKE_RULE_FILE")
-    if injected and os.isfile(injected) then
-        includes(injected)
-        return true
-    end
-
-    for _, prefix in ipairs(path.splitenv(os.getenv("AMENT_PREFIX_PATH") or "")) do
-        local rulefile = path.join(
-            prefix, "share", "ament_xmake", "xmake", "rules", "ament_xmake", "package.lua")
-        if os.isfile(rulefile) then
-            includes(rulefile)
-            return true
-        end
-    end
-    return false
-end
-
-if not import_ament_xmake_rule() then
-    raise("cannot find ament_xmake rule via AMENT_PREFIX_PATH")
-end
-
 add_rules("mode.debug", "mode.release")
 
 target("demo_xmake_cpp")
