71243f90c9ad35c6c7775c212df28bfc763068c3
sunrisepeak <speakshen@163.com>
2026-03-19 10:33:21 +0800

    refactor: switch demo xmake usage to global add_rules mode

--- demo_xmake_cpp/xmake.lua
+++ demo_xmake_cpp/xmake.lua
@@ -3,17 +3,16 @@ set_version("0.1.0")
 set_languages("cxx17")
 
 add_rules("mode.debug", "mode.release")
+add_rules("ament_xmake.package")
 
 target("demo_xmake_cpp")
     set_kind("static")
-    add_rules("ament_xmake.package")
     set_installdir("lib")
     add_files("src/demo_lib.cpp")
     add_headerfiles("src/demo_lib.hpp", {prefixdir = "include/demo_xmake_cpp"})
 
 target("demo_xmake_cpp_node")
     set_kind("binary")
-    add_rules("ament_xmake.package")
     set_installdir("lib/demo_xmake_cpp")
     add_files("src/main.cpp")
     add_deps("demo_xmake_cpp")
